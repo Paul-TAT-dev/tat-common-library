881a7cdf882dee275ee7ba2433fb4f32d1e262be
Paul-TAT-dev <paul@travelagent-tools.com>
2025-09-30 04:56:49 +0800

    - Fix onClick

--- src/tab/Tab.tsx
+++ src/tab/Tab.tsx
@@ -1,4 +1,4 @@
-import { FC, memo } from 'react';
+import { FC, memo } from "react";
 
 interface TabProps {
   value: string;
@@ -7,16 +7,14 @@ interface TabProps {
 }
 
 const Tab: FC<TabProps> = ({ value, isSelected, onClick }) => {
-  const isSelectedClass = isSelected ? 'active btnClass' : 'border-0';
+  const isSelectedClass = isSelected ? "active btnClass" : "border-0";
 
   return (
     <div
       className={`w-100 btn btn-outline-secondary ${isSelectedClass} px-3 px-md-2 py-2 d-flex`}
+      onClick={onClick}
     >
-      <div
-        className="col-12 d-flex justify-content-between align-items-center"
-        onClick={onClick}
-      >
+      <div className="col-12 d-flex justify-content-between align-items-center">
         <div className="text-start">{value}</div>
       </div>
     </div>
