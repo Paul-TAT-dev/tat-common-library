79cec9aa5fdfd14f724920e97dd91b135df30f97
Paul-TAT-dev <paul@travelagent-tools.com>
2025-11-07 05:35:08 +0800

    - Fix tab component

--- src/tab/Tab.stories.tsx
+++ src/tab/Tab.stories.tsx
@@ -16,3 +16,74 @@ export const Default: Story = {
     onClick: () => {},
   },
 };
+
+export const Success: Story = {
+  args: {
+    value: "Tab 1",
+    isSelected: false,
+    onClick: () => {},
+    status: "success",
+  },
+};
+
+export const SuccessActive: Story = {
+  args: {
+    value: "Tab 1",
+    isSelected: true,
+    onClick: () => {},
+    status: "success",
+  },
+};
+
+export const Alert: Story = {
+  args: {
+    value: "Tab 1",
+    isSelected: false,
+    onClick: () => {},
+    status: "alert",
+  },
+};
+export const AlertActive: Story = {
+  args: {
+    value: "Tab 1",
+    isSelected: true,
+    onClick: () => {},
+    status: "alert",
+  },
+};
+
+export const Error: Story = {
+  args: {
+    value: "Tab 1",
+    isSelected: false,
+    onClick: () => {},
+    status: "error",
+  },
+};
+
+export const ErrorActive: Story = {
+  args: {
+    value: "Tab 1",
+    isSelected: true,
+    onClick: () => {},
+    status: "error",
+  },
+};
+
+export const Todo: Story = {
+  args: {
+    value: "Tab 1",
+    isSelected: false,
+    onClick: () => {},
+    status: "todo",
+  },
+};
+
+export const TodoActive: Story = {
+  args: {
+    value: "Tab 1",
+    isSelected: true,
+    onClick: () => {},
+    status: "todo",
+  },
+};
--- src/tab/Tab.tsx
+++ src/tab/Tab.tsx
@@ -1,21 +1,52 @@
+import {
+  SquareAsterisk,
+  SquareCheckBig,
+  SquarePen,
+  SquareX,
+} from "lucide-react";
 import { FC, memo } from "react";
+
+import "./Tab.scss";
 
 interface TabProps {
   value: string;
   isSelected: boolean;
   onClick: () => void;
+  status?: "success" | "alert" | "error" | "todo";
 }
 
-const Tab: FC<TabProps> = ({ value, isSelected, onClick }) => {
+const Tab: FC<TabProps> = ({ value, isSelected, onClick, status }) => {
   const isSelectedClass = isSelected ? "active btnClass" : "border-0";
+
+  const renderStatusIcon = () => {
+    switch (status) {
+      case "success":
+        return (
+          <SquareCheckBig height={16} width={16} className="flex-shrink-0" />
+        );
+      case "alert":
+        return (
+          <SquareAsterisk height={16} width={16} className="flex-shrink-0" />
+        );
+      case "error":
+        return <SquareX height={16} width={16} className="flex-shrink-0" />;
+      case "todo":
+        return <SquarePen height={16} width={16} className="flex-shrink-0" />;
+      default:
+        return "";
+    }
+  };
 
   return (
     <div
-      className={`w-100 btn btn-outline-secondary ${isSelectedClass} px-3 px-md-2 py-2 d-flex`}
+      className={`tat-tab w-100 btn btn-outline-secondary ${isSelectedClass} px-3 px-md-2 py-2 d-flex tat-${status}`}
       onClick={onClick}
     >
       <div className="col-12 d-flex justify-content-between align-items-center">
-        <div className="text-start">{value}</div>
+        <div className="text-start d-flex align-items-center">
+          {renderStatusIcon()}
+          <label className="flex-grow-1 ms-1">{value}</label>
+        </div>
       </div>
     </div>
   );
